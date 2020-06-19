9c537607a6f1a5a09700ac6848a90a5c75d44f81
khatabwedaa <khatabwedaa@gmail.com>
2020-06-19 11:58:10 +0200

    Build sidebar and header

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,8 +1,9 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="flex h-screen bg-gray-200">
-    <div class="w-64 bg-gray-900 overflow-y-auto hidden lg:block">
+<div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
+    <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
+    <div :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
         <div class="flex items-center justify-center mt-8">
             <div class="flex items-center">
                 <svg class="h-12 w-12" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -40,7 +41,7 @@
     <div class="flex-1 flex flex-col">
         <header class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600">
             <div class="flex items-center">
-                <button class="text-gray-500 focus:outline-none lg:hidden">
+                <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
                     <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
